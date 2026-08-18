989a324daa23ff2d20fddf335fd58d36c81b0ede
sumit123-pixel <cmaker368@gmail.com>
2026-08-18 14:51:06 +0530

    day1
    added c++ code

---
+++ day1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    //this is comment i write in my code and it is not disturbing the code 
+    /*this is used 
+     for multi line comments*/
+    //  for direct comment to the line to use click "ctrl + /"
+    int sumit=07;
+    cout<<"Hello world , my name is sumit."<< sumit;
+    return 0;
+}
+
