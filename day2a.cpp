3960ac19d4b9f39d3c2bf9218e952ed66d8594af
sumit123-pixel <cmaker368@gmail.com>
2026-08-19 17:27:34 +0530

    day2a
    add day2a

--- day2a.cpp
+++ day2a.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main() {
-    cout<<"hello";
+    int num1 , num2;
+
+    cout<<"enter the value of num1:\n";
+    cin>>num1;
+
+    cout<<"enter the value of num2:\n";
+    cin>>num2;
+
+    cout<<"the sum is:\n"<<num1+num2<<"\n";
+
+    cout<<"the difference is:\n"<<num1-num2<<"\n";
+
     return 0;
 }
+
+//<< is called insertion operator and >> is called extraction operator.
