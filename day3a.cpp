779d89413d54d3442b0fba783b6f500ca824f786
sumit123-pixel <cmaker368@gmail.com>
2026-08-21 15:07:07 +0530

    day3a
    add day3a

--- day3a.cpp
+++ day3a.cpp
@@ -6,7 +6,7 @@ int c=8;
 
 int main(){
 int a, b, c;
-
+//built in data types are int, float, double, char, bool, void, long double, long long int, short int, unsigned int
     cout<<"hello world, this is day 3 of c++ programming"<<endl;
 
     cout<<"The value of a is:";
@@ -21,11 +21,44 @@ int a, b, c;
 
     float d=12.2f;
     long double e=12.2l;
-
+//literal suffixes are used to specify the type of a literal constant. For example, 12.2f is a float literal, while 12.2l is a long double literal. If no suffix is provided, the literal is treated as a double by default.
     cout<<"The value of d is:"<<d<<endl;
     cout<<"The value of e is:"<<e<<endl;
+    cout<<"The size of f is:"<<sizeof(12.2f)<<endl;
+    cout<<"The size of e is:"<<sizeof(12.2l)<<endl;
+    cout<<"The size of 12.2 is:"<<sizeof(12.2)<<endl;
+    cout<<"The size of F:"<<sizeof(12.2F)<<endl;
+    cout<<"The size of L is:"<<sizeof(12.2L)<<endl;
 
-     
+     //reference variable is used to give same name to another variable and it is created by using '&' operator
+
+     float x=111;
+     float& y=x;
+
+     cout<<x<<endl;
+     cout<<y<<endl;
+
+     //typecasting is used to convert one variable type into another variable type and it is done by using '()' operator
+
+     int z=12;
+    //  cout<<"The value of z is :"<<z<<endl; this gives the normal result and it is in int for converting in float or in other variable we use () 
+
+    cout<<"The value of z is :"<<(float)z<<endl;
+    //or
+    cout<<"The value of z is :"<<float(z)<<endl;
+
+    float p=56.66;
+    cout<<"the value of p is :"<<int(p)<<endl;
+
+    //we can make like this also
+    int q=int(p); 
+    // by this we can convert float p into int q and store it in int variable
+    cout<<"the value of q is:"<<q<<endl;
+
+    cout<<z+p<<endl;
+    cout<<z+int(p)<<endl;
+    cout<<z+(int)p<<endl;
+    
     return 0;
 }
 
