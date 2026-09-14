d4df375b01174b22c437c7d489e4ed48a1bf13b6
sumit123-pixel <cmaker368@gmail.com>
2026-09-14 21:56:55 +0530

    oops4.cpp
    oops4.cpp

---
+++ oops4.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+//Inheritence is a mechanism in C++ that allows a class to inherit properties and behaviors from another class. The class that inherits is called the derived class, and the class being inherited from is called the base class. Inheritance allows for code reusability and the creation of hierarchical relationships between classes.
+
+class person{
+    public:
+    string name;
+    int age;
+
+    //constructor
+    person(){
+       
+    }
+};
+
+class student: public person {
+    public:
+    int rollno;
+
+    void info(){
+        cout<<"Name:"<<name<<endl;
+        cout<<"Age:"<<age<<endl;
+        cout<<"Rollno.:"<<rollno<<endl;
+    }
+};
+
+int main(){
+
+student s1;
+s1.name="sumit";
+s1.age=18;
+s1.rollno=123;
+
+cout<<s1.name<<endl;
+cout<<s1.age<<endl;
+cout<<s1.rollno<<endl;
+
+return 0;
+}
