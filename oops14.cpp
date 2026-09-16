cdfe549f03848a1adf67e13cb50eb9a50e7cd4cc
sumit123-pixel <cmaker368@gmail.com>
2026-09-16 21:57:35 +0530

    oops14.cpp
    oops14.cpp

---
+++ oops14.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+void fun(){
+    static int x=0;
+    cout<<"x:"<<x<<endl;
+    x++;
+}
+
+int main(){
+    fun();
+    fun();
+    fun();
+
+return 0;
+}
