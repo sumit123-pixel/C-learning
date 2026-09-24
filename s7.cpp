6a3bffbd56578d45530bec748b0254ba4ae96a84
sumit123-pixel <cmaker368@gmail.com>
2026-09-24 17:53:22 +0530

    s7.cpp
    s7.cpp

--- s7.cpp
+++ s7.cpp
@@ -10,6 +10,10 @@ public:
         this->a=a;
         this->b=b;
       }
+      void setbysumdata(complex o1, complex o2){
+        a= o1.a + o2.a ;
+        b= o1.b + o2.b ;
+      }
       void printdata(void){
         cout<<"the complex no. is "<<a<<"+"<<b<<"i"<<endl;
       }
@@ -17,9 +21,15 @@ public:
 
 int main(){
 
-complex c1;
+complex c1, c2, c3;
 c1.setdata(1,2);
 c1.printdata();
+
+c2.setdata(3,4);
+c2.printdata();
+
+c3.setbysumdata(c1, c2);
+c3.printdata();
 
 return 0;
 }
